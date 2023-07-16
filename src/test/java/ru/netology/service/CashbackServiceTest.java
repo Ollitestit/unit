c3404743783c568ca8998dc9afaadbe1933b3d95
Olli <olli.konyuhova@gmail.com>
2023-07-16 19:52:04 +0500

    Revert "Tests Junit4"
    This reverts commit 0d88df62865534e3622180567bbe2eaae81d2c89.

--- src/test/java/ru/netology/service/CashbackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackServiceTest.java
@@ -1,40 +1,22 @@
 package ru.netology.service;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CashbackServiceTest {
-    @org.junit.Test
+    @Test
     public void shouldAddIfAmountBelowBoundary() {
         CashbackHackService service = new CashbackHackService();
-        org.junit.Assert.assertEquals(100, service.remain(900));
+        Assert.assertEquals(service.remain(900), 100);
     }
-
-    @org.junit.Test
+    @Test
     public void shouldAddIfAmountAboveBoundary() {
         CashbackHackService service = new CashbackHackService();
-        org.junit.Assert.assertEquals(900, service.remain(1100));
+        Assert.assertEquals(service.remain(1100), 900);
     }
-
-    @org.junit.Test
+    @Test
     public void shouldReturn0IfAmountEqualBoundary() {
         CashbackHackService service = new CashbackHackService();
-        org.junit.Assert.assertEquals(0, service.remain(1000));
+        Assert.assertEquals(service.remain(1000), 0);
     }
-
-    @org.junit.jupiter.api.Test
-    public void shouldAddIfAmountBelowBoundaryJunit5() {
-        CashbackHackService service = new CashbackHackService();
-        org.junit.jupiter.api.Assertions.assertEquals(100, service.remain(900));
-    }
-
-    @org.junit.jupiter.api.Test
-    public void shouldAddIfAmountAboveBoundaryJunit5() {
-        CashbackHackService service = new CashbackHackService();
-        org.junit.jupiter.api.Assertions.assertEquals(900, service.remain(1100));
-    }
-
-    @org.junit.jupiter.api.Test
-    public void shouldReturn0IfAmountEqualBoundaryJunit5() {
-        CashbackHackService service = new CashbackHackService();
-        org.junit.jupiter.api.Assertions.assertEquals(0, service.remain(1000));
-    }
-
 }
